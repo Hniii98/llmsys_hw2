24d39c9146db15a139368e55c0808ec39d78db38
Hniii <752608928@qq.com>
2026-02-23 16:26:40 +0800

    fix: fix NaN when calling sqrt in CUDA

--- minitorch/optim.py
+++ minitorch/optim.py
@@ -87,8 +87,9 @@ class Adam(Optimizer):
 
                     state['step'] += 1
                     state['exp_avg'] = state['exp_avg'] * self.beta1 + (1 - self.beta1) * grad
-                    state['exp_avg_sq'] = state['exp_avg_sq'] * self.beta2 + (1 - self.beta2) * grad ** 2
-
+                    #state['exp_avg_sq'] = state['exp_avg_sq'] * self.beta2 + (1 - self.beta2) * grad ** 2
+                    # Using multiplication to avoid NaN when calling CUDA sqrt with negative inputs
+                    state['exp_avg_sq'] = state['exp_avg_sq'] * self.beta2 + (1 - self.beta2) * grad * grad
                     denom = state['exp_avg_sq'] ** 0.5 + self.eps
 
                     bias_correction1 = 1. - self.beta1 ** state['step']
